127dc48e9e656cd3a4a1d23db25fef336b5fa8f9
Babadu31 <bastienmiss31@gmail.com>
2022-11-29 01:40:31 -0800

    10h37-29/11/22-0-2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,10 @@
+'''blabla'''
 #!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def web_flask():
+    '''blabla comeback'''
     return "<p>Hello HBNB!</p>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
