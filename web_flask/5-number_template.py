cb42b21bcbcc907d23e256e76fc734236edda5aa
Babadu31 <bastienmiss31@gmail.com>
2022-11-29 03:31:22 -0800

    12h31_29/11/22_5-3

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,6 +2,8 @@
 '''blabla'''
 from flask import Flask
 from flask import render_template
+
+
 app = Flask(__name__)
 
 
@@ -34,7 +36,9 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return(render_template('5-number.html', n=n))
+    if type(n) == int:
+        return render_template('5-number.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
