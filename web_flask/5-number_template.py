d4fe322cead0a6889dc9a966bd1028c5f4a50900
Babadu31 <bastienmiss31@gmail.com>
2022-11-29 03:41:20 -0800

    12h41_29/11/22_5-5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''blabla'''
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -40,4 +39,4 @@ def template(n):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
